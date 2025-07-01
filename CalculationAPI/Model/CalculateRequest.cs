a44a7bac4d08590c4576bc871c5e6c8a496cf747
Shyam Dandasena <155178915+microsamsung@users.noreply.github.com>
2025-07-01 09:21:01 +0300

    Update CalculateRequest.cs
    Validation error message

--- CalculationAPI/Model/CalculateRequest.cs
+++ CalculationAPI/Model/CalculateRequest.cs
@@ -8,10 +8,10 @@ namespace CalculationAPI.Model
         public OperationType type { get; set; }
        
         [Required]
-        [Range(0.1,double.MaxValue)]
+        [Range(0.1,double.MaxValue,ErrorMessage ="First Operand should be greater than 0")]
         public T? FirstOperand { get; set; }
         [Required]
-        [Range(0.1, double.MaxValue)]
+        [Range(0.1, double.MaxValue, ErrorMessage = "Second Operand should be greater than 0")]  
         public T? SecondOperand { get; set; }
     }
 }
